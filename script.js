9f9faa8858389abb16bc669186bcf8ab5533dd33
Evan <evanmurray2012@gmail.com>
2021-03-31 16:39:33 +0100

    added Napster API + bug fixes

--- script.js
+++ script.js
@@ -1,98 +1,144 @@
-const image = document.querySelector('img');
-const title = document.getElementById('title');
-const artist = document.getElementById('artist');
-const music = document.querySelector('audio');
-const currentTimeEl = document.getElementById('current-time');
-const durationEl = document.getElementById('duration');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+const shuffleBtn = document.getElementById("shuffle");
+const repeatBtn = document.getElementById("repeat");
 
-// Music //
-const songs = [
-  {
-    name: 'jacinto-1',
-    displayName: 'Electric Chill Machine',
-    artist: 'Jacinto Design',
-  },
-  {
-    name: 'jacinto-2',
-    displayName: 'Seven Nation Army (Remix)',
-    artist: 'Jacinto Design',
-  },
-  {
-    name: 'jacinto-3',
-    displayName: 'Goodnight, Disco Queen',
-    artist: 'Jacinto Design',
-  },
-  {
-    name: 'metric-1',
-    displayName: 'Front Row (Remix)',
-    artist: 'Metric/Jacinto Design',
-  },
-];
+// Booleans
+let shuffle = false;
+let repeat = false;
+let playing = false;
 
-// Check if Playing
-let isPlaying = false;
+let trackIndex = 0;
+const apiUrl =
+  "https://api.napster.com/v2.2/genres/g.398/tracks/top?apikey=MmU5NzI1NDctZWM0Zi00ZWVhLTg2YzMtNTIzNjNhNjRmMjI4";
+let trackArray = [];
 
 // Play
 function playSong() {
-  isPlaying = true;
-  playBtn.classList.replace('fa-play', 'fa-pause');
-  playBtn.setAttribute('title', 'Pause');
+  playing = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
   music.play();
 }
 
 // Pause
 function pauseSong() {
-  isPlaying = false;
-  playBtn.classList.replace('fa-pause', 'fa-play');
-  playBtn.setAttribute('title', 'Play');
+  playing = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
   music.pause();
 }
 
-// Play or Pause Event Listener
-playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
-
 // Update DOM
-function loadSong(song) {
-  title.textContent = song.displayName;
-  artist.textContent = song.artist;
-  music.src = `music/${song.name}.mp3`;
-  image.src = `img/${song.name}.jpg`;
+function loadSongFromApiData(trackData) {
+  if (trackData) {
+    console.log(trackData);
+
+    if (trackData.name.length > 22 || trackData.artistName.length > 25) {
+      reduceFontSize();
+    } else {
+      resetFontSize();
+    }
+
+    title.textContent = trackData.name;
+    artist.textContent = trackData.artistName;
+
+    music.src = trackData.previewURL;
+    image.src = `http://direct.rhapsody.com/imageserver/v2/albums/${trackData.albumId}/images/300x300.jpg`;
+  }
 }
 
-// Current Song
-let songIndex = 0;
+function reduceFontSize() {
+  title.classList.add("long-title");
+  artist.classList.add("long-artist");
+}
 
-// Previous Song
+function resetFontSize() {
+  title.classList.remove("long-title");
+  artist.classList.add("long-artist");
+}
+
+//fetch songs
+async function fetchSongs(url) {
+  let trackArr = [];
+  try {
+    const response = await fetch(url);
+    trackArr = await response.json();
+  } catch (error) {
+    console.log("Error fetching data :" + error);
+  }
+
+  trackArray = trackArr.tracks;
+  return trackArr.tracks;
+}
+
+// function for previous song
 function prevSong() {
-  songIndex--;
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
-  loadSong(songs[songIndex]);
+  trackIndex <= 0 ? (trackIndex = trackArray.length - 1) : trackIndex--;
+
+  loadSongFromApiData(trackArray[trackIndex]);
   playSong();
 }
 
-// Next Song
-function nextSong() {
-  songIndex++;
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
-  loadSong(songs[songIndex]);
+function repeatSong() {
+  loadSongFromApiData(trackArray[trackIndex]);
   playSong();
+}
+
+// Function for next song
+function nextSong() {
+  if (repeat) {
+    repeatSong();
+  } else {
+    trackIndex < trackArray.length - 1 ? trackIndex++ : (trackIndex = 0);
+    if (shuffle) {
+      loadSongFromApiData(
+        trackArray[Math.floor(Math.random() * trackArray.length)]
+      );
+    } else {
+      loadSongFromApiData(trackArray[trackIndex]);
+    }
+    playSong();
+  }
+}
+
+function shuffleBoolean() {
+  if (shuffle) {
+    shuffleBtn.setAttribute("style", "color: pink");
+    shuffle = false;
+  } else {
+    shuffleBtn.setAttribute("style", "color: #1DB954");
+    shuffle = true;
+  }
+}
+
+function repeatBoolean() {
+  if (repeat) {
+    repeatBtn.setAttribute("style", "color: pink");
+    repeat = false;
+  } else {
+    repeatBtn.setAttribute("style", "color: #1DB954");
+    repeat = true;
+  }
 }
 
 // On Load - Select First Song
-loadSong(songs[songIndex]);
+trackArray = fetchSongs(apiUrl).then((trackArray) => {
+  loadSongFromApiData(trackArray[0]);
+});
 
 // Update Progress Bar & Time
 function updateProgressBar(e) {
-  if (isPlaying) {
+  if (playing) {
     const { duration, currentTime } = e.srcElement;
     // Update progress bar width
     const progressPercent = (currentTime / duration) * 100;
@@ -126,8 +172,12 @@ function setProgressBar(e) {
 }
 
 // Event Listeners
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
-music.addEventListener('ended', nextSong);
-music.addEventListener('timeupdate', updateProgressBar);
-progressContainer.addEventListener('click', setProgressBar);
+music.addEventListener("ended", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
+
+playBtn.addEventListener("click", () => (playing ? pauseSong() : playSong()));
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+shuffleBtn.addEventListener("click", shuffleBoolean);
+repeatBtn.addEventListener("click", repeatBoolean);
